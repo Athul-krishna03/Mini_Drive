ca6085e5678cade038f4e36313636da76d7c9eb8
ATHUL0527 <www.athulappu0527@gmail.com>
2025-08-25 06:24:58 +0530

    minor changes

--- Backend/src/lib/s3.ts
+++ Backend/src/lib/s3.ts
@@ -1,6 +1,6 @@
 import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { env } from '../../src/config/env';
+import { env } from '../config/env';
 
 
 export const s3 = new S3Client({
